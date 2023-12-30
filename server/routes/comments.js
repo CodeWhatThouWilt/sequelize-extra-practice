aefcca08fef5ca917b4d976185410f1d749b9c96
Jacob <yakenorth@gmail.com>
2023-12-30 05:32:43 -0500

    cleared solution code

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,64 +1,6 @@
 const router = require("express").Router();
 
-const { Comment } = require("../db/models");
-
 // Your code here
-router.get("/", async (req, res, next) => {
-    const comments = await Comment.findAll({
-        attributes: ["id", "content", "createdAt", "postId"],
-        order: [["createdAt", "ASC"]],
-    });
-
-    return res.json({ comments });
-});
-
-router.post("/", async (req, res, next) => {
-    const { userId, content, postId } = req.body;
-    console.log(userId, content);
-    const newComment = await Comment.create({
-        userId,
-        content,
-        postId,
-    });
-
-    return res.json({ data: newComment });
-});
-
-router.put("/:commentId", async (req, res, next) => {
-    const { content } = req.body;
-
-    const editedComment = await Comment.findByPk(req.params.commentId);
-
-    if (!editedComment) {
-        const err = new Error("Comment not found");
-        err.status = 400;
-        return next(err);
-    }
-
-    editedComment.content = content;
-
-    await editedComment.save();
-
-    return res.json({ data: editedComment });
-});
-
-router.delete("/:commentId", async (req, res, next) => {
-    const { commentId } = req.params;
-
-    const doomedComment = await Comment.findByPk(commentId);
-
-    if (!doomedComment) {
-        const err = new Error("Comment not found");
-        err.status = 400;
-        return next(err);
-    }
-
-    await doomedComment.destroy();
-
-    return res.json({
-        message: "Successfully deleted comment",
-    });
-});
 
 // Export class - DO NOT MODIFY
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,34 +1,6 @@
 const router = require("express").Router();
 
 // your code here
-const { User } = require("../db/models");
-
-router.get("/", async (req, res, next) => {
-    const users = await User.findAll({
-        attributes: ["id", "username", "email"],
-        order: [["username", "ASC"]],
-    });
-
-    return res.json({ users });
-});
-
-router.get("/:userId", async (req, res, next) => {
-    const { userId } = req.params;
-
-    const user = await User.findByPk(userId, {
-        attributes: ["id", "username", "email", "bio"],
-    });
-
-    if (!user) {
-        const err = new Error("User not found");
-        err.status = 400;
-        return next(err);
-    }
-
-    return res.json({
-        details: user,
-    });
-});
 
 // Export class - DO NOT MODIFY
 module.exports = router;
